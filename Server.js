5e318786e910a9b43902e5a29c36183085c4d0b3
pawan <pawandaathakuni@gmail.com>
2023-11-17 07:41:06 +0530

    commit

--- Server.js
+++ Server.js
@@ -5,6 +5,26 @@ const dotenv=require('dotenv')
 dotenv.config({path:'./config/config.env'})
 
 const app=express()
+
+app.get('/api/v1/bootcamps',(req,res)=>{
+    res.status(200).json({sucess:true ,msg:'get all bootcamps'})
+})
+
+app.get('/api/v1/bootcamps/:id',(req,res)=>{
+    res.status(200).json({sucess:true ,msg:`get bootcamp${req.params.id}`})
+})
+
+app.post('/api/v1/bootcamps/',(req,res)=>{
+    res.status(200).json({sucess:true ,msg:'create new bootcamps'})
+})
+
+app.put('/api/v1/bootcamps/:id',(req,res)=>{
+    res.status(200).json({sucess:true ,msg:`update bootcamp ${req.params.id}`})
+})
+
+app.delete('/api/v1/bootcamps/:id',(req,res)=>{
+    res.status(200).json({sucess:true ,msg:`delete bootcamp ${req.params.id}`})
+})
 const PORT=process.env.PORT||5000
 
 
